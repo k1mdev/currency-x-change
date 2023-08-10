57928c09c12e0c6e8e951f670fea3e7081fe869e
Khazixi <khazixfangirl@gmail.com>
2023-08-09 20:53:51 -0400

    Added Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,56 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareGithub } from "@fortawesome/free-solid-svg-icons";
+import styles from '../styles/Footer.module.css'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 const Footer: React.FC = () => {
 
-    return (
-        <>
+  return (
+    <>
+      <footer className={styles.footer}>
+        <section className="footer-section">
+          <h1>Emmanuel K</h1>
+          <ul className={styles.list}>
+            <li className={styles.gap}>
+              <a className={styles.link}
+                href="#">
+                <FontAwesomeIcon icon={faGithub} /> {" "}
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a className={styles.link}
+                href="#">
+                <FontAwesomeIcon icon={faLinkedin} /> {" "}
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </section>
 
-
-                <section className="footer-section">
-                    <h1>Emmanuel K</h1>
-                    <nav>
-                        <ul>
-                            <FontAwesomeIcon icon={faSquareGithub} />
-                            <li><a>LinkedIn</a></li>
-                            <li><a>GitHub</a></li>
-                        </ul>
-                    </nav>
-                </section>
-
-                <section className="footer-section">
-                    <h1>Jason S</h1>
-                    <nav>
-                        <ul>
-                            <li><a>LinkedIn</a></li>
-                            <li><a>GitHub</a></li>
-                        </ul>
-                    </nav>
-                </section>
-
-        </>
-    )
+        <section className="footer-section">
+          <h1>Jason S</h1>
+          <ul className={styles.list}>
+            <li className={styles.gap}>
+              <a className={styles.link}
+                href="https://www.linkedin.com/in/jason-simmonds-a72806263">
+                <FontAwesomeIcon icon={faGithub} /> {" "}
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a className={styles.link}
+                href="https://github.com/khazixi">
+                <FontAwesomeIcon icon={faLinkedin} /> {" "}
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </section>
+      </footer>
+    </>
+  )
 
 }
 
