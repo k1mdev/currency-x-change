aca723a0972d86dbe532b9433d7c3f81ff6a0cc7
k0nem <eamonkon@gmail.com>
2023-08-11 13:32:30 -0400

    Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from '../styles/Footer.module.css'
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Montserrat } from "next/font/google";
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: '200'
+})
 
 const Footer: React.FC = () => {
 
   return (
     <>
       <footer className={styles.footer}>
+
         <section className="footer-section">
-          <h1>Emmanuel</h1>
+          <h1 className={`${styles.name} ${montserrat.className}`}>Emmanuel</h1>
           <ul className={styles.list}>
-            <li>
+            <li className={styles.gap}>
               <a className={styles.link}
-                href="#">
+                href="#" target="_blank" rel="noopener">
                 <FontAwesomeIcon icon={faGithub} /> {" "}
                 GitHub
               </a>
@@ -23,24 +29,28 @@ const Footer: React.FC = () => {
         </section>
 
         <section className="footer-section">
-          <h1>Jason S</h1>
+          <h1 className={`${styles.name} ${montserrat.className}`}>Jason</h1>
           <ul className={styles.list}>
+            
             <li className={styles.gap}>
-              <a className={styles.link}
-                href="https://www.linkedin.com/in/jason-simmonds-a72806263" target="_blank" rel="noopener">
-                 <FontAwesomeIcon icon={faLinkedin} /> {" "}
-                LinkedIn
-              </a>
-            </li>
-            <li>
               <a className={styles.link}
                 href="https://github.com/khazixi" target="_blank" rel="noopener">
                 <FontAwesomeIcon icon={faGithub} /> {" "}
                 GitHub
               </a>
             </li>
+
+            <li>
+              <a className={styles.link}
+                href="https://www.linkedin.com/in/jason-simmonds-a72806263" target="_blank" rel="noopener">
+                 <FontAwesomeIcon icon={faLinkedin} /> {" "}
+                LinkedIn
+              </a>
+            </li>
+
           </ul>
         </section>
+
       </footer>
     </>
   )
